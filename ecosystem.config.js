2e39ad0c97db2f1635eba3d2f93f0866316f0ff2
Gleb Vorontsov <gleb.vorontsov@datart.com>
2017-10-30 20:42:53 +0300

    pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name : 'wallet',
+      script : './server/index.js',
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      },
+      instances : 4,
+      exec_mode : "cluster",
+      "watch" : true,
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:kroniak/yandex-wallet.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/dev',
+      repo : 'git@github.com:kroniak/yandex-wallet.git',
+      path : '/var/www/development',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
