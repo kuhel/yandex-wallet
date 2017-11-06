f0666d0362e33e59d5e1f33394cc5f5c1a163256
Nikolay Molchanov <me@kroniak.net>
2017-11-07 00:34:06 +0300

    [edit] always drop db

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -4,4 +4,4 @@ dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 sudo cp $dir/db/dump.agz ~/yandex_wallet/db/backup/dump.agz
 
-docker exec yandex-mongodb-production /bin/bash -c 'mongorestore --gzip --archive=/backup/dump.agz --nsFrom=test_yandex_wallet.* --nsTo=yandex_wallet.*'
+docker exec yandex-mongodb-production /bin/bash -c 'mongorestore --drop --gzip --archive=/backup/dump.agz --nsFrom=test_yandex_wallet.* --nsTo=yandex_wallet.*'
